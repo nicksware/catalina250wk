6e97bb0fd362679df68c383a2768b509c8361719
nick <noobping@users.noreply.github.com>
2023-06-11 22:28:02 +0200

    1100px

--- assets/menu.js
+++ assets/menu.js
@@ -19,7 +19,7 @@ const setupEventListeners = () => {
 
     document.querySelector('nav h2').addEventListener('click', () => {
         const menu = document.querySelector('nav ul');
-        if (window.innerWidth <= 800) {
+        if (window.innerWidth <= 1100) {
             menu.style.display = menu.style.display == 'block' ? 'none' : 'block';
         }
     });
